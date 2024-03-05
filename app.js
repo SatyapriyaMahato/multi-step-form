610ef7359530081dd807308b00fde26116f42521
SatyapriyaMahato <satyapriyamahto@gmail.com>
2024-03-05 22:08:31 +0530

    add second step

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const emailId = document.getElementById('user_email'),
     userPhone = document.getElementById('user_phone'),
-    prevBtn = document.getElementById('prev_btn'),
     nextBtn = document.getElementById('next_btn'),
-    form = document.getElementById('form_container');
+    prevBtn = document.getElementById('prev_btn'),
+    form = document.getElementById('step_1');
 
 nextBtn.addEventListener('click', e => {
     const emailText = emailId.value.trim();
@@ -17,9 +17,13 @@ nextBtn.addEventListener('click', e => {
     }
 
 });
+
+prevBtn.addEventListener('click', e => {
+    form.style.display = "block";
+})
 function setSuccess() {
-    const form = document.getElementById('form_container');
     form.style.display = "none";
+    prevBtn.style.visibility = "visible";
     success_div = document.getElementById('div_success');
     success_div.style.display = "block";
 }
@@ -39,3 +43,19 @@ const validateEmail = (email) => {
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         );
 };
+
+
+const checkbox = document.getElementById('switch-rounded'),
+    monthlyParagraph = document.querySelector('.renewal p:first-child'),
+    yearlyParagraph = document.querySelector('.renewal p:last-child');
+
+checkbox.addEventListener('change', e => {
+    if (checkbox.checked) {
+        yearlyParagraph.style.color = 'hsl(213, 96%, 18%)';
+        monthlyParagraph.style.color = 'hsl(231, 11%, 63%)';
+    } else {
+        monthlyParagraph.style.color = 'hsl(213, 96%, 18%)';
+        yearlyParagraph.style.color = 'hsl(231, 11%, 63%)';
+
+    }
+});
