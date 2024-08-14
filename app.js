65aa00d5d0e7de9e6247399b943f650ad92b9694
SatyapriyaMahato <satyapriyamahto@gmail.com>
2024-08-14 20:55:16 +0530

    Add buttons functionality

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const userName = document.getElementById('user_name'),
 
 
 
-// Start Step 1
-
+//---------------------- Step 1 check ---------------------- //
 const toggleError = (elementId, show) => {
     document.getElementById(elementId).style.display = show ? "block" : "none";
 };
@@ -37,11 +36,9 @@ const validateMobileNumber = (number) => {
     return mobileNumberPattern.test(number);
 };
 
-// end step 1
 
 
-
-// start step 2
+//---------------------- Step 2 check ---------------------- //
 let plan_amount = 0;
 plan.forEach(p => {
     p.addEventListener('click', e => {
@@ -61,7 +58,8 @@ function selectedPlan() {
 };
 
 
-// slider switch 
+//---------------------- Monthly or yearly switch ---------------------- //
+
 const checkbox = document.getElementById('switch-rounded'),
     monthlyParagraph = document.querySelector('.renewal p:first-child'),
     yearlyParagraph = document.querySelector('.renewal p:last-child');
@@ -86,12 +84,9 @@ if (yearly) {
 
 
 
-//end step 2
 
+//---------------------- Step 3 check ---------------------- //
 
-
-//----------------start step 3 ------------//
-// add ons
 const add_ons = document.querySelectorAll(".add_on");
 let add_on_amount = 0;
 add_ons.forEach(addOn => {
@@ -106,16 +101,10 @@ add_ons.forEach(addOn => {
 
             if (checkbox.checked) {
                 closestAddOn.style.borderColor = "var(--Purplish-blue)";
-
-
                 add_on_amount += closestAddOn_amount;
-                console.log(closestAddOn_amount);
-                console.log(add_on_amount);
             } else {
                 closestAddOn.style.borderColor = "var(--Light-gray)";
                 add_on_amount -= closestAddOn_amount;
-                console.log(closestAddOn_amount);
-                console.log(add_on_amount);
             }
         }
     });
@@ -126,51 +115,7 @@ add_ons.forEach(addOn => {
 //----------------Btn functionality------------//
 let count = 1;
 let steps = [false, false, false, false];
-nextBtn.addEventListener('click', e => {
 
-    //------------- step 1 check -----------//
-    e.preventDefault();
-
-    userName.value === "" ? setNameError() : removeNameError();
-    !validateMobileNumber(userPhone.value) ? setPhoneError() : removePhoneError();
-
-    const emailText = emailId.value.trim();
-    !validateEmail(emailText) ? setEmailError() : removeEmailError();
-
-    const allErrorsClear = () =>
-        ['label_EmailError', 'label_PhoneError', 'label_NameError']
-            .every(id => document.getElementById(id).style.display === "none");
-
-    const step_one = allErrorsClear();
-    if (step_one) {
-        steps[0] = true;
-        count++;
-        document.getElementById('step_1').classList.add('hidden');
-        document.getElementById('step_2').classList.remove('hidden');
-        prevBtn.style.visibility = "visible";
-
-        toggleStepNumber(count % 5);
-
-    }
-
-    if (plan_amount > 0) {
-        steps[1] = true;
-        count++;
-        document.getElementById('step_2').classList.add('hidden');
-        document.getElementById('step_3').classList.remove('hidden');
-        prevBtn.style.visibility = "visible";
-
-        toggleStepNumber(count % 5);
-    }
-
-    //------------- step 2 check -----------//
-
-
-    //------------- step 3 check -----------//
-    //------------- step 4 check -----------//
-
-
-});
 
 function setSuccess() {
     form.style.display = "none";
@@ -185,7 +130,6 @@ function hidePrev(s) {
 }
 
 function hideNext(s) {
-    console.log(s);
     const step = document.getElementById(`step_${s}`);
     const step2 = document.getElementById(`step_${--s}`);
     step.classList.add("hidden");
@@ -193,23 +137,67 @@ function hideNext(s) {
 }
 
 function toggleStepNumber(s) {
-    console.log(s);
     const activeNum = document.querySelector(`.number_${s}`);
     const pastNum = document.querySelector(`.number_${--s}`);
     activeNum.classList.add("done");
     pastNum.classList.remove("done");
 }
+function toggleRevStepNumber(s) {
+    console.log(s);
+    const activeNum = document.querySelector(`.number_${s}`);
+    const pastNum = document.querySelector(`.number_${++s}`);
+    activeNum.classList.add("done");
+    pastNum.classList.remove("done");
+}
+nextBtn.addEventListener('click', e => {
+    console.log(count);
+    e.preventDefault();
 
+    if (count == 1) {
+        userName.value === "" ? setNameError() : removeNameError();
+        !validateMobileNumber(userPhone.value) ? setPhoneError() : removePhoneError();
+
+        const emailText = emailId.value.trim();
+        !validateEmail(emailText) ? setEmailError() : removeEmailError();
+
+        const allErrorsClear = () =>
+            ['label_EmailError', 'label_PhoneError', 'label_NameError']
+                .every(id => document.getElementById(id).style.display === "none");
+
+        const step_one = allErrorsClear();
+        if (step_one) {
+            steps[0] = true;
+            count++;
+            document.getElementById('step_1').classList.add('hidden');
+            document.getElementById('step_2').classList.remove('hidden');
+            prevBtn.style.visibility = "visible";
+            toggleStepNumber(count);
+        }
+    } else if (count == 2) {
+        if (plan_amount > 0) {
+            steps[1] = true;
+            document.getElementById('step_2').classList.add('hidden');
+            document.getElementById('step_3').classList.remove('hidden');
+            count++;
+            toggleStepNumber(count);
+        }
+    } else if (count == 3) {
+        steps[2] = true;
+        document.getElementById('step_3').classList.add('hidden');
+        document.getElementById('step_4').classList.remove('hidden');
+        nextBtn.style.visibility = "hidden";
+        count++;
+        toggleStepNumber(count);
+
+    }
+});
 
 prevBtn.addEventListener('click', e => {
     if (count == 2) prevBtn.style.visibility = "hidden";
-    hideNext(count % 5);
+
+    hideNext(count);
+    if (count < 4) nextBtn.style.visibility = "visible";
     count--;
+    steps[count] = false;
+    toggleRevStepNumber(count);
 })
-
-
-
-
-
-
-//start step 4
